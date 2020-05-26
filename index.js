ac06899afde01d541388e5dc7a2d70836ec016a3
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-05-25 20:13:15 -0700

    setup first query

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const { Toolkit } = require('actions-toolkit')
+const {Toolkit} = require("actions-toolkit");
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  tools.exit.success('We did it!')
-})
+  const issueTitles = await fetchIssueData(tools);
+
+  tools.exit.success("We did it!");
+});
+
+async function fetchIssueData(tools) {
+  const repoOwner = tools.context.payload.repository.owner.login;
+  const repoName = tools.context.payload.repository.name;
+
+  const query = `query ($owner: String!) { 
+   {
+        repository(owner: "${repoOwner}", name:"${repoName}") {
+          name
+          owner {
+            login
+          }
+          issues(states: OPEN, first: 100) {
+            totalCount
+            edges {
+              node {
+                title
+                labels(first: 3) {
+                  edges {
+                    node {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+  }`;
+
+  const result = await tools.github.graphql(query);
+  return result;
+}
